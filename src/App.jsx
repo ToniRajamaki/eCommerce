6c8f967e5ab7b14bab6259d3e0826aba58535164
Toni Rajamäki <zingoo16@gmail.com>
2024-05-04 09:44:30 +0300

    created some placeholder sections for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    NAV
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      HERO
+    </section>
+    
+    <section className="padding">
+      Popular producs
+    </section>
+     
+    <section className="padding">
+      Superquality
+    </section>
+     
+    <section className="padding-x">
+      Services
+    </section>
+     
+    <section className="padding">
+      SpecialOffer
+    </section>
+
+    <section className="padding bg-pale-blue">
+      CustomerReviews
+    </section>
+
+    <section className="padding-x sm:py-32 py-16 w-full">
+    Subscribe
+    </section>
+
+    <section className="padding bg-black padding-x padding-t pb-8">
+      Footer
+    </section>
+    
+
+  </main>
+)
+
+export default App;
+ 
